e87dfa5c6eb32c32b151ebcd937ee0bb401e5ddc
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2024-01-14 10:52:11 +0530

    Initial commit

---
+++ src/test/java/com/javaspring/online_quiz_application/OnlineQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaspring.online_quiz_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineQuizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
