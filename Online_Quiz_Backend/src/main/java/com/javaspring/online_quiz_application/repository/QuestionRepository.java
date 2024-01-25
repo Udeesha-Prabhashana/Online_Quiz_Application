4dd4f42524910633b1e9147381db9fecb3cfa06a
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2024-01-25 23:36:11 +0530

    Added all methods in the QuestionService

--- Online_Quiz_Backend/src/main/java/com/javaspring/online_quiz_application/repository/QuestionRepository.java
+++ Online_Quiz_Backend/src/main/java/com/javaspring/online_quiz_application/repository/QuestionRepository.java
@@ -1,8 +1,16 @@
 package com.javaspring.online_quiz_application.repository;
 
 import com.javaspring.online_quiz_application.model.Question;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.net.ContentHandler;
+import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question , Long> {
 
+    List<String> findDistinctSubject();
+
+    Page<Question> findBySubject(String subject, Pageable pageable);
 }
--- Online_Quiz_Backend/src/main/java/com/javaspring/online_quiz_application/service/IQuestionService.java
+++ Online_Quiz_Backend/src/main/java/com/javaspring/online_quiz_application/service/IQuestionService.java
@@ -1,6 +1,7 @@
 package com.javaspring.online_quiz_application.service;
 
 import com.javaspring.online_quiz_application.model.Question;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,7 +16,7 @@ public interface IQuestionService {
 
     List<String> getAllSubjects();
 
-    Question updateQuestion(Long id, Question question);
+    Question updateQuestion(Long id, Question question) throws ChangeSetPersister.NotFoundException;
 
     void deleteQuestion(Long id);
 
