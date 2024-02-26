04709fed43f1f2cadff46d5621cdbd720928f81c
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2024-02-26 19:47:14 +0530

    Partially completed GetAllQuestion

--- Online_Quiz_Frontend/Components/quiz/GetAllQuiz.jsx
+++ Online_Quiz_Frontend/Components/quiz/GetAllQuiz.jsx
@@ -44,7 +44,37 @@ const GetAllQuiz = () => {
 	}
 
   return (
-    <div></div>
+      <section className="container">
+          <div className="row mt-5">
+              <div className="col-md-6 mb-2 md-mb-0" style={{color: "GrayText"}}>
+                  <h4> All Questions</h4>
+              </div>
+              <div className="col-md-4 d-flex justify-content-end">
+                  {/* Todo : add a link to navigate to add new question form */}
+              </div>             
+          </div>
+          <hr />
+          {isQuestionDeleted && <div className='alert alert-success'>{deleteSuccess}</div>}
+          {questions.map((question, index) => {
+              <div>           
+                  <h4 style={{ color: "GrayText" }}> {`${ index + 1 }. ${ question.question }`} </h4>
+                  
+                  <ul>
+                      {question.choices((choice, index) => {
+                          <li key={index}>{ choice}</li>
+                      })}
+                  </ul>
+                  <p className='text'>Correct Answer: {question.correctAnswers}</p>
+                  <div className='btn-group mb-4'>
+                      {/* Todo : add a link to navigate to update question form */}
+                      <button className='btn btn-outline-danger btn-sm'
+                      onClick={()=>(handleDelete(question.id))}>
+                        Delete Question
+                      </button>
+                  </div>
+              </div>
+          })}
+      </section>
   )
 }
 
